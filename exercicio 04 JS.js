b73111257cb0efa66da765326f7ba5138008c868
fjavier02 <franciscolopez1995@gmail.com>
2020-10-29 21:42:37 -0300

    adicionando pruebas del exercicio 04

---
+++ exercicio 04 JS.js
@@ -0,0 +1,95 @@
+//document.querySelector('#resultado') //para seleccionar la sección de resultado
+//let resultado = document.querySelector('#resultado')
+
+//resultado.innerHTML = '<br> agora soy um paragrafo </br>' //modifica el valos del div para gregar resultado
+
+/* let novaPessoa = document.createElement('li') //agregar nuevos nombres a la lista
+novaPessoa.textContent = 'dado de pessoa'
+resultado.appendChild(novaPessoa) */
+
+/* let pessoasMaisVelhas = document.createElement('p') //agregar os dados das pessoas mais valhas
+pessoasMaisVelhas.textContent = 'dado de pessoa'
+resultado.appendChild(pessoasMaisVelhas) */
+
+let Pessoas = []
+
+
+/* let nome = document.querySelector('#Nome') 
+
+let idade 
+    while (true) {
+    idade = Number(document.querySelector('#idade'))
+     if (! Number.isNaN(idade)) {
+        break // detener while se não foi um numero
+    }
+    alert ('Por favor digite um numero') 
+}*/
+
+let pessoa = {
+        nome, idade
+}
+
+
+
+
+let MaisVelhos = obterMaisVelhos(Pessoas) //pessoa o pessoas mais velha
+
+function obterMaisVelhos(Pessoas) {
+    let MaisVelhos = [ Pessoas[0] ]
+    for(let registro of Pessoas.slice(1)) {
+        if (registro.idade > MaisVelhos[0].idade) {
+            MaisVelhos = [ registro ]
+        }
+        else if (registro.idade === MaisVelhos[0].idade) {
+            MaisVelhos.push(registro)
+        }
+    }
+    return MaisVelhos
+}
+
+function logValidation() {
+    console.log('el boton esta funcionando');
+}
+
+function limparDados() {
+    resultado.innerHTML = '<br> Registro2: </br>' ;
+
+}
+//console.log ('Las personas registrada foram:')
+//console.log (' ')
+function adicionarDado() {
+    let nome = document.querySelector('#nome');
+    let idade = document.querySelector('#idade');
+    console.log(nome.value);
+    console.log(idade.value); //verificar por que da log, pero no guarda la variable nome e idade
+    let pessoa = {
+        nome: nome.value,
+        idade: idade.value
+    }
+    Pessoas.push(pessoa)
+}
+
+if (MaisVelhos.length > 1) {
+    for (registro of MaisVelhos) {
+        let pessoasMaisVelhas = document.createElement('p') //mostrar lista das pessoas mais velhas
+        pessoasMaisVelhas.textContent = `São os maiores com ${registro.idade}`
+        resultado.appendChild(pessoasMaisVelhas)
+        //console.log (`${registro.nome} `)
+    }
+    let pessoasMaisVelhas = document.createElement('p') //mostrar idade das pessoas mais velhas
+    pessoasMaisVelhas.textContent = `${registro.nome} `
+    resultado.appendChild(pessoasMaisVelhas)
+
+    /* console.log (`São os maiores com ${registro.idade}`)
+    console.log (' ') */
+}
+
+else {
+    for (registro of MaisVelhos) {
+        let pessoasMaisVelhas = document.createElement('p') //mostrar nome e idade da pessoa mais velha
+        pessoasMaisVelhas.textContent = `${registro.nome} foi o mais velho com ${registro.idade}`
+        resultado.appendChild(pessoasMaisVelhas)    
+        //console.log (`${registro.nome} foi o mais velho com ${registro.idade}` )
+    }
+    
+}
---
+++ scrip.js
@@ -0,0 +1,11 @@
+function adicionarDado() {
+    let nome = document.querySelector('#nome');
+    let idade = document.querySelector('#idade');
+    console.log(nome.value);
+    console.log(idade.value);
+}
+
+function limparDados() {
+    console.log('click')
+}
+
