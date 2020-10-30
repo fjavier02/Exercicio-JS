d31b4a1120f169d6469ee6cef558d5be17baf3bb
fjavier02 <franciscolopez1995@gmail.com>
2020-10-30 04:04:14 -0300

    ejecicio completo

--- exercicio 04 JS.js
+++ exercicio 04 JS.js
@@ -1,41 +1,21 @@
-//document.querySelector('#resultado') //para seleccionar la sección de resultado
-//let resultado = document.querySelector('#resultado')
-
-//resultado.innerHTML = '<br> agora soy um paragrafo </br>' //modifica el valos del div para gregar resultado
-
-/* let novaPessoa = document.createElement('li') //agregar nuevos nombres a la lista
-novaPessoa.textContent = 'dado de pessoa'
-resultado.appendChild(novaPessoa) */
-
-/* let pessoasMaisVelhas = document.createElement('p') //agregar os dados das pessoas mais valhas
-pessoasMaisVelhas.textContent = 'dado de pessoa'
-resultado.appendChild(pessoasMaisVelhas) */
-
 let Pessoas = []
 
-
-/* let nome = document.querySelector('#Nome') 
-
-let idade 
-    while (true) {
-    idade = Number(document.querySelector('#idade'))
-     if (! Number.isNaN(idade)) {
-        break // detener while se não foi um numero
-    }
-    alert ('Por favor digite um numero') 
-}*/
 
 let pessoa = {
         nome, idade
 }
+let mayores = []
+let registro = []
 
 function adicionarDado() {
     let nome = document.querySelector("#nome");
     let idade = document.querySelector("#idade");
     console.log(nome);
     console.log(idade);
-    pessoa.nome = nome.value;
-    pessoa.idade = idade.value;
+    let pessoa = {
+        nome: nome.value,
+        idade:idade.value
+    }
     console.log(pessoa);
     Pessoas.push(pessoa);
     let persona = document.createElement("li");
@@ -44,31 +24,60 @@ function adicionarDado() {
     let lista = document.querySelector("#lista");
     lista.appendChild(persona);
     console.log(lista);
-    let MaisVelhos = obterMaisVelhos(Pessoas)
+    mayores = obterMaisVelhos(Pessoas);
+    console.log(mayores);
+   
+    //let mayor = document.createElement("p");
+    //mayor.innerHTML = `${mayores.nome} tiene ${mayores.idade}`; 
+    registro = mayores [0];
+    console.log(registro);
+    console.log(document.querySelector("#comparacion"));
+    let pessoasMaisVelhas = document.querySelector("#comparacion");
+    pessoasMaisVelhas.innerHTML = `${registro.nome} foi o mais velho com ${registro.idade}`
+
+
+    /* document.querySelector('#comparacion').removefirstChild.appendChild(pessoasMaisVelhas); */
+    
+/*    if (mayores.length > 1) {
+        for (registro of mayores ) {
+            let pessoasMaisVelhas = document.createElement('p') //mostrar lista das pessoas mais velhas
+            pessoasMaisVelhas.innerHTML = `A pessoa mais velhas são ${registro.nome} com ${registro.idade} anos <br>`
+        }
+        let pessoasMaisVelhas = document.createElement('p') //mostrar idade das pessoas mais velhas
+        pessoasMaisVelhas.innerHTML = `${registro.nome} ` 
+    }
+    
+    else {
+        
+        resultado.appendChild(pessoasMaisVelhas) 
+        //console.log (`${registro.nome} foi o mais velho com ${registro.idade}` 
+        
+    }*/
 }
 
  //pessoa o pessoas mais velha
 
-function obterMaisVelhos(Pessoas) {
-    let MaisVelhos = [ Pessoas[0] ]
-    for(let registro of Pessoas.slice(1)) {
-        if (registro.idade > MaisVelhos[0].idade) {
+function obterMaisVelhos() {
+    let MaisVelhos = [ Pessoas[0] ] //igualar al primer valor de Pessoas
+    for(let registro of Pessoas.slice(1)) { // comparar con los demas valores de Pessoas
+        if (registro.idade > MaisVelhos[0].idade) { //si es mayor igual al valor
             MaisVelhos = [ registro ]
         }
-        else if (registro.idade === MaisVelhos[0].idade) {
+        else if (registro.idade === MaisVelhos[0].idade) { //si es igual adiciona 
             MaisVelhos.push(registro)
         }
+        //console.log(registro);
     }
     return MaisVelhos
 }
 
 
 function limparDados() {
-    resultado.innerHTML = '<h3> Registro2: </h3>' ;
-
+    resultado.innerHTML = ' <br> <h3> Registro: </h3> </br><br> <ul id="lista"> </ul> </br><br> <p id="comparacion"></p> </br>' ; //restablece os valores de la sección como estan en HTML  
+    while (Pessoas.length !== 0) { //reseteando lista de personas
+        Pessoas.pop();
+    }
 }
-//console.log ('Las personas registrada foram:')
-//console.log (' ')
 
 
 /* if (MaisVelhos.length > 1) {
@@ -82,12 +91,9 @@ function limparDados() {
     pessoasMaisVelhas.textContent = `${registro.nome} `
     resultado.appendChild(pessoasMaisVelhas)
 
-    /* console.log (`São os maiores com ${registro.idade}`)
-    console.log (' ') */
-//}
+}
 
-
-/* else {
+else {
     for (registro of MaisVelhos) {
         let pessoasMaisVelhas = document.createElement('p') //mostrar nome e idade da pessoa mais velha
         pessoasMaisVelhas.textContent = `${registro.nome} foi o mais velho com ${registro.idade}`
